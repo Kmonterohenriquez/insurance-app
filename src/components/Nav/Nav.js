ce5ce7350cb1954d1827867c88bca196f5c115c8
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-04 14:27:09 -0700

    Nav component imported into Home view

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,13 @@ import './Nav.sass'
 const Nav = () => {
     return (
         <div className='Nav'>
-            
+            <img src="" alt="" className="logo"/>
+            <nav>
+                <ul>
+                    <li className="nav-link">For you & family</li>
+                    <li className="nav-link">For Business</li>
+                </ul>
+            </nav>
         </div>
     )
 }
