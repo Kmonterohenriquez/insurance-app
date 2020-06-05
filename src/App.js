489132669a464038f21104199d1b398b3cd1345f
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-04 19:11:41 -0700

    Showcase component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Nav from './components/Nav/Nav'
-import './App.sass';
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import "./App.sass";
+import Showcase from "./components/Showcase/Showcase";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-
+      <Nav />
+      <Showcase />
     </div>
   );
 }
--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import './Showcase.sass'
-const Showcase = () => {
-    return (
-        <div className='Showcase'>
-            
-        </div>
-    )
-}
+import React from "react";
+import "./Showcase.sass";
+import showcase_img from "../../img/showcase_img.svg";
 
-export default Showcase
+const Showcase = () => {
+  return (
+    <div className="Showcase">
+      <div className="Showcase-container container">
+        <div className="left-side">
+          <h3>Hello, We are fincorp</h3>
+          <h1>Insurance <br/> made easy.</h1>
+          <button>Get your free Quote</button>
+        </div>
+        <div className="right-side">
+          <img src={showcase_img} alt="showcase" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Showcase;
