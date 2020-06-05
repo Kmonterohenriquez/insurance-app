a202cfbf23f5b32274fceabf53ff994655c3e9e0
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-04 20:06:57 -0700

    Basic structure of Plans component is done

---
+++ src/components/Plans/Plans.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Plans.sass'
+
+const Plans = () => {
+    return (
+        <div className='Plans'>
+            
+        </div>
+    )
+}
+
+export default Plans
