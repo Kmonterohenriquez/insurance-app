e728a8877546683aa3aa93f26fe518917503609e
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-08 23:07:51 -0400

    Structure of Guarantee component is done

--- src/components/Guarantee/Guarantee.js
+++ src/components/Guarantee/Guarantee.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import './Guarantee.sass'
+import React from "react";
+import "./Guarantee.sass";
+import shield from "../../img/shield-yellow.svg";
 const Guarantee = () => {
-    return (
-        <div className='Guarantee'>
-            
-        </div>
-    )
-}
+  return (
+    <div className="Guarantee">
+      <img src={shield} alt="yellow shield" />
+      <div className="left-side">
+        <h1>100% Satisfaction Guarantee</h1>
+        <p>
+          We offer no questions asked refund policy for 14 days from the policy
+          date.
+        </p>
+      </div>
+      <div className="right-side">
+        <button>get a free Quote</button>
+      </div>
+    </div>
+  );
+};
 
-export default Guarantee
+export default Guarantee;
