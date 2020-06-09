a053c0e65b27f6b28f3d5f5ad9f262312b6e07c3
Kevin Montero <kevinwilding1997@gmail.com>
2020-06-08 23:03:42 -0400

    Basic structure of Guarantee component

---
+++ src/components/Guarantee/Guarantee.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Guarantee.sass'
+const Guarantee = () => {
+    return (
+        <div className='Guarantee'>
+            
+        </div>
+    )
+}
+
+export default Guarantee
